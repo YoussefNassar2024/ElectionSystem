ad85a14efdc3705a6182fb83616228afe17402ec
Youssef Ahmed <94936068+YoussefNassar2024@users.noreply.github.com>
2023-12-14 20:53:34 +0200

    add required data screen finished

--- vote/lib/main.dart
+++ vote/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vote/firebase_options.dart';
 import 'package:vote/home.dart';
+import 'package:vote/screens/create_poll/add_required_data_screen.dart';
 import 'package:vote/screens/history/card.dart';
 import 'package:vote/screens/create_poll/add_candidates_screen.dart';
 import 'package:vote/screens/history/history_screen.dart';
@@ -20,14 +21,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Directionality(
-            textDirection: TextDirection.ltr,
-            child: HistoryCard(
-              pollName: 'NU Poll',
-              date: '12/12/2023',
-              winnerName: 'Youssef Ahmed',
-              winPercentage: 62,
-              photoUrl:
-                  "https://firebasestorage.googleapis.com/v0/b/election-37353.appspot.com/o/94936068.jpg?alt=media&token=3c9ee6f6-8059-4587-9b3d-209db6454eba",
-            )));
+            textDirection: TextDirection.ltr, child: AddRequiredData()
+            //     HistoryCard(
+            //   pollName: 'NU Poll',
+            //   date: '12/12/2023',
+            //   winnerName: 'Youssef Ahmed',
+            //   winPercentage: 62,
+            //   photoUrl:
+            //       "https://firebasestorage.googleapis.com/v0/b/election-37353.appspot.com/o/94936068.jpg?alt=media&token=3c9ee6f6-8059-4587-9b3d-209db6454eba",
+            // )
+            ));
   }
 }
