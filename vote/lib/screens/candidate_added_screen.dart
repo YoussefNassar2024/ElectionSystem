2c794b9a5028696e4b92a574d1ea3f8d5951350c
Youssef Ahmed <94936068+YoussefNassar2024@users.noreply.github.com>
2023-12-18 21:38:45 +0200

    misc

--- vote/lib/screens/candidate_added_screen.dart
+++ vote/lib/screens/candidate_added_screen.dart
@@ -22,15 +22,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: CandidateAddedScreen(title: 'Flutter Demo Home Page'),
+      home: CandidateAddedScreen(),
     );
   }
 }
 
 class CandidateAddedScreen extends StatefulWidget {
-  const CandidateAddedScreen({super.key, required this.title});
-
-  final String title;
+  const CandidateAddedScreen({
+    super.key,
+  });
 
   @override
   State<CandidateAddedScreen> createState() => AddCandidatesScreenState();
@@ -40,61 +40,63 @@ class AddCandidatesScreenState extends State<CandidateAddedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image.asset(
-          'assets/images/shareCode.png',
-          width: MediaQuery.of(context).size.width * 0.8,
-        ),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Container(
-            height: 350,
-            width: 300,
-            color: CustomStyle.colorPalette.purple,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Text("Candidates Saved!",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500)),
-                const Text("Share the poll using this code: ",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400)),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Container(
-                    height: 100,
-                    width: 200,
-                    color: CustomStyle.colorPalette.lightPurple, /*child:*/
-                  ),
-                )
-              ],
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/images/shareCode.png',
+            width: MediaQuery.of(context).size.width * 0.8,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              height: 350,
+              width: 300,
+              color: CustomStyle.colorPalette.purple,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text("Candidates Saved!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500)),
+                  const Text("Share the poll using this code: ",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400)),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Container(
+                      height: 100,
+                      width: 200,
+                      color: CustomStyle.colorPalette.lightPurple, /*child:*/
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        customButton(
-          context: context,
-          onPressed: () {
-            print("Next page");
-          },
-          childText: "Done",
-          color: CustomStyle.colorPalette.lightPurple,
-          width: 100,
-          height: 50,
-          textStyle: const TextStyle(color: Colors.black, fontSize: 30),
-        ),
-      ],
+          customButton(
+            context: context,
+            onPressed: () {
+              print("Next page");
+            },
+            childText: "Done",
+            color: CustomStyle.colorPalette.lightPurple,
+            width: 100,
+            height: 50,
+            textStyle: const TextStyle(color: Colors.black, fontSize: 30),
+          ),
+        ],
 
-      //customVerticalSpace(context: context),
+        //customVerticalSpace(context: context),
+      ),
     ));
   }
 }
